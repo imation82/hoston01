d91cedc40371247d01aadb9ea4759e0df157cdb0
Ashfaqur Rahman <odesk.rahman@gmail.com>
2022-03-01 18:41:40 +0600

    Owl carousel made workable

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,3 +13,30 @@ $(document).ready(function () {
 
 });
 
+$(".testimonial-slider").owlCarousel({	
+    items : 3,
+    margin: 20,
+    smartSpeed: 1000,
+    slideSpeed : 500,
+    mouseDrag: true,
+    dots: true,
+	loop: true,
+    responsiveClass:true,      
+    autoplay : true,
+    nav: false,
+    responsive:{
+        0:{
+            items: 1
+        },
+        481:{
+            items: 1
+        },
+        768:{
+            items: 2
+        },
+        992:{
+            items: 3
+        }
+    }
+});
+
